c0c9c5a8fbe6f482e66587b9f1f072c848c81316
sajjaddoodabi <sajad.d@arianalabs.io>
2023-07-06 19:15:49 +0330

    add blank product list API

--- design/views.py
+++ design/views.py
@@ -1,13 +1,19 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, BlankProduct
+from .serializers import CategorySerializer, BlankProductSerializer
+
+
 class ListCategoryAPI(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-
 class RetrieveCategoryAPI(RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ListBLankProductAPI(ListAPIView):
+    queryset = BlankProduct.objects.all()
+    serializer_class = BlankProductSerializer
