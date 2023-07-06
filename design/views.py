6c8a0f698ebdc267e83a746f66695f8b7be88efc
sajjaddoodabi <sajad.d@arianalabs.io>
2023-07-06 20:16:18 +0330

    add filter back end and pagination, fields to retrieve blank product API -should be filled

--- design/views.py
+++ design/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Category, BlankProduct
 from .serializers import CategorySerializer, BlankProductSerializer, BlankProductRetrieveSerializer
@@ -20,5 +23,10 @@ class ListBLankProductAPI(ListAPIView):
 
 
 class RetrieveBLankProductAPI(RetrieveAPIView):
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = []
+    ordering_fields = []
+    pagination_class = PageNumberPagination
+
     queryset = BlankProduct.objects.all()
     serializer_class = BlankProductRetrieveSerializer
