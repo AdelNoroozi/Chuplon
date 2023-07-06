7e23a205d64397068002e1e18adb43ab696a7269
sajjaddoodabi <sajad.d@arianalabs.io>
2023-07-06 18:39:02 +0330

    add retrieve and list API for category

--- design/views.py
+++ design/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+class ListCategoryAPI(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+
+class RetrieveCategoryAPI(RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
