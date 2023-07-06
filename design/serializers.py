b910eeafecf712da4e690e6291ab5ee51f9b634d
sajjaddoodabi <sajad.d@arianalabs.io>
2023-07-06 19:15:39 +0330

    add blank product serializer that return average price of each product

--- design/serializers.py
+++ design/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, BlankProduct, BlankProductProviderProp
+
 
 class CategorySerializer(serializers.ModelSerializer):
     children = serializers.SerializerMethodField()
@@ -12,3 +13,20 @@ class CategorySerializer(serializers.ModelSerializer):
         children = Category.objects.filter(parent=obj)
         serializer = self.__class__(children, many=True)
         return serializer.data
+
+
+class BlankProductSerializer(serializers.ModelSerializer):
+    average_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = BlankProduct
+        fields = ("title", "desc", "props", "category", "average_price")
+
+    def get_average_price(self, obj):
+        prices = BlankProductProviderProp.objects.filter(blank_product_id=obj.id).values("price")
+        average_price = 0
+        for price in prices:
+            average_price += int(price["price"])
+        average_price /= len(prices)
+
+        return average_price
