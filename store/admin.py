30722e57b9bf7938476328dd6ae8332ab881ab6e
Mjavadrahimi <manre80@gmail.com>
2023-07-07 06:07:01 +0330

    admin store-Order added

--- store/admin.py
+++ store/admin.py
@@ -14,3 +14,19 @@ class DiscountAdmin(admin.ModelAdmin):
     )
     search_fields = ("title",)
 
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "customer",
+        "total_price",
+        "discount",
+        "address",
+        "status",
+        "post_tracking_code",
+
+    )
+    search_fields = ("customer", "status", "post_tracking_code",)
+
+
